4b6e4f9e9c929cc3e123bc84eb07fac5cabb38c2
devmargooo <devmargooo@gmail.com>
2025-02-04 14:18:59 +0300

    user entity, bcryptjs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,8 @@ import { ILogger } from '../logger/logger.interface';
 import 'reflect-metadata';
 import { IUsersController } from './users.interface';
 import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegisterDto } from './dto/user-register.dto';
+import { User } from './user.entity';
 
 @injectable()
 export class UserController extends BaseController implements IUsersController {
@@ -22,7 +24,9 @@ export class UserController extends BaseController implements IUsersController {
 		this.ok(res, 'login');
 	}
 
-	register(req: Request, res: Response): void {
-		this.ok(res, 'register');
+	async register({ body }: Request<any, any, UserRegisterDto>, res: Response): Promise<void> {
+		const user = new User(body.email, body.name);
+		await user.setPassword(body.password);
+		this.ok(res, user);
 	}
 }
